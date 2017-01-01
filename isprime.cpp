64c60a1279317fb14c4b3653066d17cbb2c8b382
John Selbie <jselbie@gmail.com>
2017-01-01 02:30:16 -0800

    Fix comment so it's more clear

--- isprime.cpp
+++ isprime.cpp
@@ -30,7 +30,7 @@ bool isPrime(long long n)
     Any column with an "x" is already a value divisible by 3 or 5. So we can skip several sequences
     So instead of testing against every odd number, we can test against numbers that are not divisible by 2,3,or5
     003,005,007,009,011,013,015,017,019,021,023,025,027,029,031,033,035,037,039,041,043,045,047,049,051
-    0   2   4   6   8  10  12  14  16  18  20  22  24         *30
+    0             2   4   6   8  10  12  14  16  18  20  22  24         *30
     3     x           x           x           x           x           x           x           x           x
     5         x                   x                   x                   x                   x
     */
